4e4dd639bc628f77787061f53549864e4a484665
diegogcast <diego.guerrero1@catolica.edu.sv>
2026-09-10 09:50:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let num = 255;
+
+console.log("El tipo de dato de la variable es: " + typeof num);
+
+console.log("El valor de la variable es: " + num);
+
+let elementoTipo = document.querySelector('#tipo');
+let elementoValor = document.querySelector('#valor');
+
+elementoTipo.innerHTML = "El tipo de la variable es: <strong>" + typeof num + "</strong>";
+
+elementoValor.innerHTML = "El valor de la variable es: <strong>" + num + "</strong>";
+
+document.bodyapendChild(document.createElement('div'));
+let elementohtml= document.querySelector('div');
+elementohtml = backgroundcolor 
